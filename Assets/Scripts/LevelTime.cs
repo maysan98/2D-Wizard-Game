bad20d9ee785de7b18a463e0ac128649a0dcf4ab
maysan98 <Maisansaleh1@gmail.com>
2026-02-10 21:22:12 +0300

    Added Level Time script

---
+++ Assets/Scripts/LevelTime.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class LevelTime : MonoBehaviour
+{
+
+    [SerializeField] GameObject textObject;
+    private TextMeshProUGUI timeText;
+    [SerializeField] int levelDuration = 30;
+    private bool countingSeconds = false;
+
+
+
+    void Start()
+    {
+        timeText = textObject.GetComponent<TextMeshProUGUI>();
+    }
+
+    void Update()
+    {   
+        timeText.text = " Time Left = " + levelDuration;
+
+        if (levelDuration != 0 && countingSeconds == false)
+        {
+        StartCoroutine(Timer());
+        } else if (levelDuration == 0)
+        {
+            timeText.text = " Time UP! ";
+            // Respawn()
+        }
+        
+
+        
+    }
+
+    IEnumerator Timer()
+    {  countingSeconds = true;
+        yield return new WaitForSeconds(1);
+        levelDuration -= 1;
+        countingSeconds = false;
+    }
+
+}
